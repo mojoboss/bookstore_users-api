55c12876fa289098958334ec37c4fb043670c0f4
mojoboss <animusmojo@gmail.com>
2020-02-12 23:25:32 +0530

    added user domain

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mojoboss/bookstore_users-api/controllers/users"
 )
 
-func mapUrls(){
+func mapUrls() {
 	router.GET("/ping", ping.Ping)
 	router.GET("/users/:user_id", users.GetUser)
 	router.POST("/users", users.CreateUser)
--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,11 +1,28 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mojoboss/bookstore_users-api/domain/users"
+	"io/ioutil"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		//TODO: HANDLE THIS
+		fmt.Println(err.Error())
+		return
+	}
+	if err = json.Unmarshal(bytes, &user); err != nil {
+		//TODO: HANDLE THIS
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(user)
 	c.String(http.StatusNotImplemented, "later")
 }
 
