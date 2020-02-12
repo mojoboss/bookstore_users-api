a2f2c408da76880dea7a4e67515c50b11a1eedfc
mojoboss <animusmojo@gmail.com>
2020-02-12 23:48:08 +0530

    added user service

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/mojoboss/bookstore_users-api/domain/users"
+	"github.com/mojoboss/bookstore_users-api/services"
 	"io/ioutil"
 	"net/http"
 )
@@ -22,8 +23,13 @@ func CreateUser(c *gin.Context) {
 		fmt.Println(err.Error())
 		return
 	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: HANDLE THIS
+		return
+	}
 	fmt.Println(user)
-	c.String(http.StatusNotImplemented, "later")
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,9 @@
 package services
+
+import (
+	"github.com/mojoboss/bookstore_users-api/domain/users"
+)
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
