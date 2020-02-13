85c985c00f9f2331b2b2510c1a849bd408da2c16
mojoboss <animusmojo@gmail.com>
2020-02-13 20:15:54 +0530

    added errors package

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,31 +1,23 @@
 package users
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/mojoboss/bookstore_users-api/domain/users"
 	"github.com/mojoboss/bookstore_users-api/services"
-	"io/ioutil"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		//TODO: HANDLE THIS
-		fmt.Println(err.Error())
-		return
-	}
-	if err = json.Unmarshal(bytes, &user); err != nil {
-		//TODO: HANDLE THIS
-		fmt.Println(err.Error())
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err)
+		//TODO:HANDLE ERROR return bad request
 		return
 	}
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		//TODO: HANDLE THIS
+		//TODO: HANDLE THIS user creation error
 		return
 	}
 	fmt.Println(user)
---
+++ utils/errors/rest_errors.go
@@ -0,0 +1,4 @@
+package errors
+
+type RestErr struct {
+}
