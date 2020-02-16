3d6341eb3638f5d42fb51988a38afbea9d6eb7e2
mojoboss <animusmojo@gmail.com>
2020-02-16 12:45:52 +0530

    added adte utils

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/mojoboss/bookstore_users-api/utils/date_utils"
 	"github.com/mojoboss/bookstore_users-api/utils/errors"
 )
 
@@ -30,6 +31,7 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
 	userDB[user.Id] = user
 	return nil
 }
