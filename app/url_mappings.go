3888e09aa96c8563c8ad6a4b6b418c20b8469559
mojoboss <animusmojo@gmail.com>
2020-02-19 23:02:18 +0530

    added patch method

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -10,4 +10,5 @@ func mapUrls() {
 	router.GET("/users/:user_id", users.GetUser)
 	router.POST("/users", users.CreateUser)
 	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
--- services/user_service.go
+++ services/user_service.go
@@ -23,14 +23,26 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
 }
 
-func UpdateUser(user users.User) (*users.User, *errors.RestErr) {
+func UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) {
 	current, err := GetUser(user.Id)
 	if err != nil {
 		return nil, err
 	}
-	current.Firstname = user.Firstname
-	current.LastName = user.LastName
-	current.Email = user.Email
+	if isPartial {
+		if user.Firstname != "" {
+			current.Firstname = user.Firstname
+		}
+		if user.LastName != "" {
+			current.LastName = user.LastName
+		}
+		if user.Email != "" {
+			current.Email = user.Email
+		}
+	} else {
+		current.Firstname = user.Firstname
+		current.LastName = user.LastName
+		current.Email = user.Email
+	}
 	if err = current.Update(); err != nil {
 		return nil, err
 	}
