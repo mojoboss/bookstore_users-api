5b13b83b6d4c7c001050d148c833ef048d25fe2b
mojoboss <animusmojo@gmail.com>
2020-02-24 00:41:39 +0530

    adding service logs

--- app/application.go
+++ app/application.go
@@ -2,13 +2,15 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mojoboss/bookstore_users-api/logger"
 )
 
-var(
+var (
 	router = gin.Default()
 )
 
 func StartApplication() {
 	mapUrls()
+	logger.Info("starting the application..")
 	router.Run(":8080")
 }
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,25 +2,24 @@ package users
 
 import (
 	"github.com/mojoboss/bookstore_users-api/datasources/postgres/users_db"
+	"github.com/mojoboss/bookstore_users-api/logger"
 	"github.com/mojoboss/bookstore_users-api/utils/errors"
-	"github.com/mojoboss/bookstore_users-api/utils/postgres_utils"
-	"log"
 	"time"
 )
 
 func (user *User) Get() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare("SELECT * FROM users_db.get_user($1)")
 	if err != nil {
-		log.Println("Error in db prepare for get user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in Prepare get_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	defer stmt.Close()
 	var firstName, lastName, email, status, password string
 	var creationTime time.Time
 	err = stmt.QueryRow(user.Id).Scan(&firstName, &lastName, &email, &status, &password, &creationTime)
 	if err != nil {
-		log.Println("Error in scanning get user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in Query row in get_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	user.Firstname = firstName
 	user.LastName = lastName
@@ -34,14 +33,14 @@ func (user *User) Get() *errors.RestErr {
 func (user *User) Save() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare("SELECT * FROM users_db.insert_user($1, $2, $3, $4, $5)")
 	if err != nil {
-		log.Println("Error in db prepare for save user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in Prepare insert_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(user.Firstname, user.LastName, user.Email, user.Status, user.Password)
 	if err != nil {
-		log.Println("Error in db exec for save user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in exec for insert_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	return nil
 }
@@ -49,14 +48,14 @@ func (user *User) Save() *errors.RestErr {
 func (user *User) Update() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare("SELECT * FROM users_db.update_user($1, $2, $3, $4, $5, $6)")
 	if err != nil {
-		log.Println("Error in db prepare for save user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in db prepare for update_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(user.Id, user.Firstname, user.LastName, user.Email, user.Status, user.Password)
 	if err != nil {
-		log.Println("Error in db exec for save user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in db exec for update_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	return nil
 }
@@ -64,19 +63,22 @@ func (user *User) Update() *errors.RestErr {
 func (user *User) Search(status string) ([]User, *errors.RestErr) {
 	stmt, err := users_db.Client.Prepare("SELECT * FROM users_db.search_user($1)")
 	if err != nil {
-		return nil, errors.NewInternalServerError(err.Error())
+		logger.Error("Error in search_user prepare", err)
+		return nil, errors.NewInternalServerError("database error")
 	}
 	defer stmt.Close()
 	rows, err := stmt.Query(status)
 	if err != nil {
-		return nil, errors.NewInternalServerError(err.Error())
+		logger.Error("Error in search_user query", err)
+		return nil, errors.NewInternalServerError("database error")
 	}
 	defer rows.Close()
 	users := make([]User, 0)
 	for rows.Next() {
 		var usr User
 		if err := rows.Scan(&usr.Id, &usr.Firstname, &usr.LastName, &usr.Email, &usr.Status, &usr.DateCreated); err != nil {
-			return nil, postgres_utils.HandlePQError(err)
+			logger.Error("Error in scan of search_user", err)
+			return nil, errors.NewInternalServerError("database error")
 		}
 		users = append(users, usr)
 	}
@@ -89,14 +91,14 @@ func (user *User) Search(status string) ([]User, *errors.RestErr) {
 func (user *User) Delete() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare("SELECT * FROM users_db.delete_user($1)")
 	if err != nil {
-		log.Println("Error in db prepare for delete user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in db prepare for delete_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(user.Id)
 	if err != nil {
-		log.Println("Error in db exec for delete user", err)
-		return postgres_utils.HandlePQError(err)
+		logger.Error("Error in db exec for delete_user", err)
+		return errors.NewInternalServerError("database error")
 	}
 	return nil
 }
