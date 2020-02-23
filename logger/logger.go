a5d5acc90a7fed2546b5ced4db1d9f4444401391
mojoboss <animusmojo@gmail.com>
2020-02-23 20:59:55 +0530

    added logger package

---
+++ logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	Log *zap.Logger
+)
+
+func init() {
+	LogConfig := zap.Config{
+		Level:    zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding: "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseColorLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+		OutputPaths: []string{"stdout"},
+	}
+	var err error
+	if Log, err = LogConfig.Build(); err != nil {
+		panic(err)
+	}
+}
